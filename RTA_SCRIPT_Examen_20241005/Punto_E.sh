b0cb5fb4bd83c3508e7555b9ec70481749677db3
NatyLaFlacaaa <nataliarodiciooo@gmail.com>
2024-10-07 19:16:08 +0000

    ADD: Agrego Punto E

--- RTA_SCRIPT_Examen_20241005/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_E.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd UTNFRA_SO_1P2C_2024_Turtl/RTA_ARCHIVOS_Examen_20241001
+cd UTNFRA_SO_1P2C_2024_Rodicio/RTA_ARCHIVOS_Examen_20241001
 cat /proc/meminfo
 grep MemTotal proc/meminfo > Filtro_Basico.txt
 sudo dmidecode -t chassis
